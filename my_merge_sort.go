ba332f43988b7d4bd9a86d237dc3a23fed3e33f7
Ekspresso <cost.cu4erov@mail.ru>
2022-09-10 18:58:24 +0300

    add normal merge_sort

--- my_merge_sort.go
+++ my_merge_sort.go
@@ -1,14 +1,14 @@
-package my_sort
+// package my_sort
 
-// package main
+package main
 
-// import "fmt"
+import "fmt"
 
-// func main() {
-// 	arr := [13]int{1, -99, 0, -86, 5, 11, 9, -5, -37, 121, 96, 525, 879}
-// 	ret := myMergeSort(arr[:])
-// 	fmt.Println(ret)
-// }
+func main() {
+	arr := [13]int{1, -99, 0, -86, 5, 11, 9, -5, -37, 121, 96, 525, 879}
+	ret := myMergeSort(arr[:])
+	fmt.Println(ret)
+}
 
 // func myMergeSort(arr []int) []int {
 // 	if len(arr) < 2 {
@@ -44,69 +44,27 @@ package my_sort
 // }
 
 func myMergeSort(arr []int) []int {
-	ret := make([]int, len(arr))
-	for i := 0; i < len(arr); i++ {
-		ret[i] = arr[i]
+	if len(arr) < 2 {
+		return arr
 	}
-	if len(arr) > 1 {
-		for !check(ret) {
-			ret = merge(ret)
-		}
-		return ret
-	} else {
-		return ret
-	}
+	c := len(arr) / 2
+	left := myMergeSort(arr[:c])
+	right := myMergeSort(arr[c:])
+	return merge(left, right)
 }
 
-// Функция создаёт новый массив и заполняет его поэлементно
-// в порядке возрастания элементами из 2 частей исходного массива
-func merge(arr []int) []int {
-	left, right := cutArr(arr)
-	ret := make([]int, len(left)+len(right))
-	i := 0
+func merge(left, right []int) []int {
+	ret := make([]int, 0, len(left)+len(right))
 	for len(left) > 0 && len(right) > 0 {
 		if left[0] <= right[0] {
-			ret[i] = left[0]
+			ret = append(ret, left[0])
 			left = left[1:]
 		} else {
-			ret[i] = right[0]
+			ret = append(ret, right[0])
 			right = right[1:]
 		}
-		i++
 	}
-	for j := 0; j < len(left); j++ {
-		ret[i] = left[j]
-		i++
-	}
-	for j := 0; j < len(right); j++ {
-		ret[i] = right[j]
-		i++
-	}
+	ret = append(ret, left...)
+	ret = append(ret, right...)
 	return ret
-}
-
-// Функция возвращает результат разбиения массива на 2 подмассива
-func cutArr(arr []int) ([]int, []int) {
-	centre := len(arr) / 2
-	left := make([]int, len(arr)-centre)
-	right := make([]int, centre)
-	j := 0
-	for i := 0; i < len(arr); i++ {
-		left[j] = arr[i]
-		if i < len(arr)-1 {
-			i++
-			right[j] = arr[i]
-		}
-		j++
-	}
-	return left, right
-}
-
-func check(arr []int) bool {
-	for i := 0; i < len(arr)-1; i++ {
-		if arr[i] > arr[i+1] {
-			return false
-		}
-	}
-	return true
 }
